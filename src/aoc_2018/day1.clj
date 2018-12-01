9acdc0948a62c4415a690ae516eca85d81d66a0e
espang <eike.spang@gmail.com>
2018-12-01 18:57:21 +0000

    adds solution for part2

--- src/aoc_2018/day1.clj
+++ src/aoc_2018/day1.clj
@@ -9,7 +9,30 @@
        (map read-string)
        (reduce +)))
 
+(defn first-duplicate-in [values]
+  (reduce (fn [acc value]
+            (if (contains? acc value)
+              (reduced value)
+              (conj acc value)))
+          #{}
+          values))
+
+(defn frequencies [lines]
+  (->> lines
+       cycle
+       (map read-string)
+       (reductions +)))
+
 (defn run_part1 []
   (let [input-file "resources/day1.txt"
         result (reduce-file input-file lines-handler)]
     (println "the result is" result)))
+
+(defn run_part2 []
+  (let [input-file "resources/day1.txt"
+        reduce-fn (fn [lines]
+                    (let [frequencies (frequencies lines)
+                          result (first-duplicate-in frequencies)]
+                      result))
+        first-duplicate (reduce-file input-file reduce-fn)]
+    (println "the result is" first-duplicate)))
