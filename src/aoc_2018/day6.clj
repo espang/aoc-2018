068bf93e1f1607f6be7e58b289116cfa4658ab05
espang <eike.spang@gmail.com>
2018-12-08 10:13:29 +0000

    add solution for day6

--- src/aoc_2018/day6.clj
+++ src/aoc_2018/day6.clj
@@ -1,5 +1,8 @@
 (ns aoc-2018.day6)
 
+(defn manhattan-distance [t1 t2]
+  (reduce #(+ %1 (Math/abs %2)) 0 (map - t1 t2)))
+
 (def input (slurp "resources/day6.txt"))
 
 (defn line->tuple [l]
@@ -12,71 +15,34 @@
   (->> (line->tuple line)
        (map txt->int)))
 
-(def centers
+(defn starting-points []
   (let [lines  (clojure.string/split-lines input)
-        tuples (map handle-line lines)
-        min-x  (apply min-key #(first %) tuples)
-        max-x  (apply max-key #(first %) tuples)
-        min-y  (apply min-key #(second %) tuples)
-        max-y  (apply max-key #(second %) tuples)]
-    (println min-x min-y max-x max-y)
-    tuples))
+        points (into #{} (map handle-line lines))]
+    points))
 
-(defn manhattan-distance [t1 t2]
-  (reduce #(+ %1 (Math/abs %2)) 0 (map - t1 t2)))
-;
-; ; pointed search: search d1, d2, d3 ...
-; ; vs: calculate all distances
-; (defn distances [tuples]
-;     (reduce (fn [acc val]
-;               (let [[x y t] val
-;                     distance (manhattan-distance [x y] t)]
-;                 (update-in acc
-;                            [[x y]]
-;                            (fnil #(conj %1 {:distance distance
-;                                             :tuple t})
-;                                  []))))
-;             {}
-;             (for [x (range 40 354)
-;                   y (range 41 355)
-;                   t tuples]
-;               [x y t])))
-;
-; (defn best [coll]
-;   (let [sorted (sort-by :distance coll)
-;         f (first sorted)
-;         s (second sorted)]
-;     (if (= (:distance f) (:distance s))
-;       :undefined
-;       (:tuple f))))
-;
-; (defn distances->best [distances]
-;   (reduce (fn [acc val]
-;             (conj acc [(first val) (best (second val))]))
-;           {}
-;           distances))
-
-(defn possible-points [x-min x-max y-min y-max]
-  (into #{}
-        (for [x (range x-min (inc x-max))
-              y (range y-min (inc y-max))]
-          [x y])))
-
-(defn start-points []
-  [[0 0] [2 4] [4 1] [1 1]])
+(defn find-corners [points]
+  (let [min-x  (first (apply min-key #(first %) points))
+        max-x  (first (apply max-key #(first %) points))
+        min-y  (second (apply min-key #(second %) points))
+        max-y  (second (apply max-key #(second %) points))]
+    [min-x max-x min-y max-y]))
 
 (defn neighbours [point]
  (into #{} (map #(map + point %) [[-1 0] [0 -1] [1 0] [0 1]])))
 
 (defn reduce-points [result left distance last-distance-points]
   (let [n-result (reduce (fn [acc point]
-                           (let [n (filter #(contains? left %) (neighbours point))]
+                           (let [root-p (get-in result [point :point])
+                                 n      (filter #(contains? left %) (neighbours point))]
                              (reduce (fn [sub-acc n-point]
                                        (if (contains? sub-acc n-point)
-                                         (assoc sub-acc n-point :undefined)
-                                         (assoc sub-acc n-point {:distance distance :point (get-in result [point :point])})))
-                                     acc
-                                     n)))
+                                         ; the point already exists - check if we come from the same root
+                                         (if (= root-p (get-in sub-acc [n-point :point]))
+                                           (assoc sub-acc n-point {:distance distance :point root-p})
+                                           (assoc sub-acc n-point :undefined))
+                                         (assoc sub-acc n-point {:distance distance :point root-p})))
+                                     n
+                                     acc)))
                          result
                          last-distance-points)
         handled-points (into #{} (keys n-result))
@@ -86,42 +52,55 @@
       n-result
       (recur n-result left (inc distance) n-points))))
 
-(defn find-distances [points x-min x-max y-min y-max]
-  ""
-  (let [result      ()
-        area-points (possible-points x-min x-max y-min y-max)]
+(defn create-initial-result [points]
+  (reduce (fn [acc val]
+            (assoc acc val {:distance 0 :point val}))
+          {}
+          points))
 
-    (into {} (for [p (filter #(contains? area-points %) points)] [p 0]))))
+(defn possible-points [x-min x-max y-min y-max]
+  (into #{}
+        (for [x (range x-min (inc x-max))
+              y (range y-min (inc y-max))]
+          [x y])))
 
+(defn bounds [x-min x-max y-min y-max]
+  (clojure.set/union
+    (into #{}
+          (for [x [x-min x-max]
+                y (range y-min (inc y-max))]
+            [x y]))
+    (into #{}
+          (for [x (range x-min (inc x-max))
+                y [y-min y-max]]
+            [x y]))))
 
+(defn answer1 []
+  (let [starting-points           (starting-points)
+        [min-x max-x min-y max-y] (find-corners starting-points)
+        result                    (create-initial-result starting-points)
+        possible-points           (possible-points min-x max-x min-y max-y)
+        possible-points           (clojure.set/difference possible-points starting-points)
+        result                    (reduce-points result possible-points 1 starting-points)
+        points-on-the-border      (bounds min-x max-x min-y max-y)
+        infinite-points           (reduce conj #{} (map #(get-in result [% :point]) points-on-the-border))
+        groups                    (reduce #(update %1 (:point (second %2)) (fnil inc 0)) {} result)
+        finite-points             (filter #(not (contains? infinite-points (key %))) groups)
+        answer                    (last (sort-by second finite-points))]
+    answer))
 
-(defn answer1 [points]
-  ""
-  (find-distances points 40 353 41 354))
+(defn sum-of-distance [point points distance-fn]
+  (reduce #(+ %1 (distance-fn point %2)) 0 points))
 
-(defn next-step [possible-points distance points]
-  "Returns a map of possible-point to [distance point]
-   {[0 0] [1 [1 0]]
-    [2 0] [1 [1 0]]
-    [1 1] :undefined}
-   by
-   find all neighbours of all points, if only neighbour to one
-   update the distance map, if neighbour to more update undefined"
-  (let [neighbours]))
+(defn into-map [all-points coordinates]
+  (into {}
+        (map (fn [i] [i (sum-of-distance i coordinates manhattan-distance)])
+             all-points)))
 
-
-; matrix
-; 40/41     353/41
-;
-; 40/354    353/354
-; aaaaa.cccc
-; aAaaa.cccc
-; aaaddecccc
-; aadddeccCc
-; ..dDdeeccc
-; bb.deEeecc
-; bBb.eeee..
-; bbb.eeefff
-; bbb.eeffff
-; bbb.ffffFf
-;
+(defn answer2 []
+  (let [starting-points           (starting-points)
+        [min-x max-x min-y max-y] (find-corners starting-points)
+        possible-points           (possible-points min-x max-x min-y max-y)
+        valid-locations           (filter #(< (val %) 10000)
+                                          (into-map possible-points starting-points))]
+    (count valid-locations)))
